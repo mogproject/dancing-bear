33ad9b90099b41343474496f3d657c95cd09c7ce
Yosuke Mizutani <mogproj@gmail.com>
2017-11-12 22:45:58 -0500

    beat count display wip

--- src/dancing_bear/gui_controller.py
+++ src/dancing_bear/gui_controller.py
@@ -12,12 +12,13 @@ class GUIController:
         self.executable = executable
         self.tab_id = tab_id
         self.cmd_prefix = [executable, 'execute']
+        self.beat = 0
 
     def play_downbeat(self):
-        pass
+        self._play_beat(1)
 
     def play_upbeat(self):
-        pass
+        self._play_beat(min(4, self.beat + 1))
 
     def send_bpm(self, bpm):
         self._execute_command(['document.getElementById("bpm").innerHTML = "%s";' % bpm])
@@ -25,8 +26,12 @@ class GUIController:
     def stop_play(self):
         self.send_bpm('???')
 
+    def _play_beat(self, next_beat):
+        self._execute_command(['document.getElementById("beat%d").click();' % next_beat])
+        self.beat = next_beat
+
     def _execute_command(self, cmd):
         capture_command(args=self.cmd_prefix + cmd + ['-t', self.tab_id])
-    
+
     def close(self):
         capture_command(args=[self.executable, 'close', '-w'])
